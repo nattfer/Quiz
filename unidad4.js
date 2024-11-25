30076ec921a6d4808212a5188faf8f36c2711f9b
nattfer <Natalia.feria.delacruz@gmail.com>
2024-11-25 21:01:55 +0100

    Se sube el proyecto

---
+++ unidad4.js
@@ -0,0 +1,190 @@
+let btn = document.getElementById('btn');
+let caja = document.getElementById('caja');
+let caja2 = document.getElementById('caja2');
+let caja3 = document.getElementById('caja3');
+let caja4 = document.getElementById('caja4');
+let caja5 = document.getElementById('caja5');
+let caja6 = document.getElementById('caja6');
+let caja7 = document.getElementById('caja7');
+let caja8 = document.getElementById('caja8');
+let caja9 = document.getElementById('caja9');
+let caja10 = document.getElementById('caja10');
+let caja11 = document.getElementById('caja11');
+let caja12 = document.getElementById('caja12');
+let caja13 = document.getElementById('caja13');
+let caja14 = document.getElementById('caja14');
+let caja15 = document.getElementById('caja15');
+let caja16 = document.getElementById('caja16');
+let caja17 = document.getElementById('caja17');
+let contador=0;
+
+function cambio(){
+    if(contador==0){
+        caja.classList.add('blanca');
+        contador=1;
+    } else {caja.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio2(){
+    if(contador==0){
+        caja2.classList.add('blanca');
+        contador=1;
+    } else {caja2.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio3(){
+    if(contador==0){
+        caja3.classList.add('blanca');
+        contador=1;
+    } else {caja3.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio4(){
+    if(contador==0){
+        caja4.classList.add('blanca');
+        contador=1;
+    } else {caja4.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio5(){
+    if(contador==0){
+        caja5.classList.add('blanca');
+        contador=1;
+    } else {caja5.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio6(){
+    if(contador==0){
+        caja6.classList.add('blanca');
+        contador=1;
+    } else {caja6.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio7(){
+    if(contador==0){
+        caja7.classList.add('blanca');
+        contador=1;
+    } else {caja7.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio8(){
+    if(contador==0){
+        caja8.classList.add('blanca');
+        contador=1;
+    } else {caja8.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio9(){
+    if(contador==0){
+        caja9.classList.add('blanca');
+        contador=1;
+    } else {caja9.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio10(){
+    if(contador==0){
+        caja10.classList.add('blanca');
+        contador=1;
+    } else {caja10.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio11(){
+    if(contador==0){
+        caja11.classList.add('blanca');
+        contador=1;
+    } else {caja11.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio12(){
+    if(contador==0){
+        caja12.classList.add('blanca');
+        contador=1;
+    } else {caja12.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio13(){
+    if(contador==0){
+        caja13.classList.add('blanca');
+        contador=1;
+    } else {caja13.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio14(){
+    if(contador==0){
+        caja14.classList.add('blanca');
+        contador=1;
+    } else {caja14.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio15(){
+    if(contador==0){
+        caja15.classList.add('blanca');
+        contador=1;
+    } else {caja15.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio16(){
+    if(contador==0){
+        caja16.classList.add('blanca');
+        contador=1;
+    } else {caja16.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+function cambio17(){
+    if(contador==0){
+        caja17.classList.add('blanca');
+        contador=1;
+    } else {caja17.classList.remove('blanca');
+        contador=0;
+    }   
+}
+
+btn.addEventListener('click',cambio,true);
+btn2.addEventListener('click',cambio2,true);
+btn3.addEventListener('click',cambio3,true);
+btn4.addEventListener('click',cambio4,true);
+btn5.addEventListener('click',cambio5,true);
+btn6.addEventListener('click',cambio6,true);
+btn7.addEventListener('click',cambio7,true);
+btn8.addEventListener('click',cambio8,true);
+btn9.addEventListener('click',cambio9,true);
+btn10.addEventListener('click',cambio10,true);
+btn11.addEventListener('click',cambio11,true);
+btn12.addEventListener('click',cambio12,true);
+btn13.addEventListener('click',cambio13,true);
+btn14.addEventListener('click',cambio14,true);
+btn15.addEventListener('click',cambio15,true);
+btn16.addEventListener('click',cambio16,true);
+btn17.addEventListener('click',cambio17,true);
